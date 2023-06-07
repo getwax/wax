adb993821ca8d4754cd2a4c28e0550b087f83934
Andrew Morris <voltrevo@gmail.com>
2023-06-07 06:58:22 +0000

    Simplify check

--- compression/test/VLQ.t.sol
+++ compression/test/VLQ.t.sol
@@ -6,12 +6,24 @@ import "../src/VLQ.sol";
 
 contract VLQTest is Test {
     function test_0x00_is_0() public {
-        (uint256 value, ) = this.decode(hex"00");
-
-        assertEq(value, 0);
+        check(hex"00", 0);
     }
 
-    function decode(bytes calldata stream) public pure returns (uint256, bytes calldata) {
+    function check(bytes memory input, uint256 output) internal {
+        (uint256 value, bytes memory stream) = this.decode(input);
+
+        assertEq(value, output);
+        assertEq(stream.length, 0);
+    }
+
+    /**
+     * This indirection allows making an external call, which converts bytes
+     * memory into the required bytes calldata.
+     */
+    function decode(bytes calldata stream) public pure
+        returns (uint256, bytes calldata)
+    {
         return VLQ.decode(stream);
     }
 }
+
