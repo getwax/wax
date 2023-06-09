984be46c66730dbecf5f408fdf72b6333c74ab9e
Andrew Morris <voltrevo@gmail.com>
2023-06-09 00:01:59 +0000

    Consistent solidity versions and pragmas

--- compression/test/AddressRegistry.t.sol
+++ compression/test/AddressRegistry.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import {AddressRegistry} from "../src/AddressRegistry.sol";
