272fe9bce9311cab2cad396c220d8213a9d76db8
Andrew Morris <voltrevo@gmail.com>
2023-07-17 13:43:38 +1000

    Separate lib build

--- inpage/vite.config.ts
+++ inpage/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  if (process.env.LIB) {
+    return {
+      plugins: [react()],
+      build: {
+        lib: {
+          entry: path.resolve(
+            path.dirname(import.meta.url.slice('file://'.length)),
+            './src/index.ts',
+          ),
+          name: 'WaxInPage',
+          fileName: 'inpage',
+        }
+      }
+    };
+  }
+
+  return {
+    plugins: [react()],
+  };
+});
