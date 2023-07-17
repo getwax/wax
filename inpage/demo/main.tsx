8dbf0170765d83a892892fbc180a79aedae83dcb
Andrew Morris <voltrevo@gmail.com>
2023-07-18 09:44:13 +1000

    global.ts

--- inpage/demo/main.tsx
+++ inpage/demo/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import WaxInPage from '../src';
-
-WaxInPage.global();
+import '../src/global.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ inpage/src/global.ts
@@ -0,0 +1,3 @@
+import WaxInPage from '.';
+
+WaxInPage.global();
