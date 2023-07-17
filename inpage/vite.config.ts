7c55c1768b34b35ccab0a3ceef69008828ca61c0
Andrew Morris <voltrevo@gmail.com>
2023-07-18 09:55:20 +1000

    Add globalScript build

--- inpage/vite.config.ts
+++ inpage/vite.config.ts
@@ -1,10 +1,32 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'demo-dist',
-  },
+export default defineConfig(() => {
+  if (process.env.BUILD_GLOBAL_SCRIPT) {
+    return {
+      plugins: [react()],
+      build: {
+        lib: {
+          entry: path.resolve(
+            path.dirname(import.meta.url.slice('file://'.length)),
+            './src/global.ts',
+          ),
+          formats: ['iife'],
+          name: 'WaxInPage',
+          fileName: 'waxInPage',
+        },
+        outDir: 'globalScript-dist',
+      },
+    };
+  }
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'demo-dist',
+    },
+  };
 });
