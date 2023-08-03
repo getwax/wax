4ee400db7ce4d0e3a5dbf062b501e31ef27b4160
Blake Duncan <blakecduncan@gmail.com>
2023-08-03 10:34:27 -0400

    Adding kernel BLS validator and test with build errors

---
+++ kernel-plugins/src/BLSValidator.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.4 <0.9.0;
+
+import "kernel/src/validator/IValidator.sol";
+import "openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
+import "kernel/src/utils/KernelHelper.sol";
+import "account-abstraction/samples/bls/lib/hubble-contracts/contracts/libs/BLS.sol";
+
+contract BLSValidator is IKernelValidator {
+    bytes32 public constant BLS_DOMAIN = keccak256("eip4337.bls.domain");
+    
+    event OwnerChanged(address indexed kernel, uint256[4] indexed oldPublicKey, uint256[4] indexed newPublicKey);
+
+    mapping(address => uint256[4]) public blsValidatorStorage;
+
+    function disable(bytes calldata) external override {
+        delete blsValidatorStorage[msg.sender];
+    }
+
+    function enable(bytes calldata _data) external override {
+        require(_data.length >= 128, "Calldata is not long enough for bls public key");
+
+        uint256[4] memory publicKey = abi.decode(_data, (uint256[4]));
+        uint256[4] memory oldPublicKey = blsValidatorStorage[msg.sender];
+        blsValidatorStorage[msg.sender] = publicKey;
+
+        emit OwnerChanged(msg.sender, oldPublicKey, publicKey);
+    }
+
+    function validateUserOp(UserOperation calldata _userOp, bytes32 _userOpHash, uint256)
+        external
+        view
+        override
+        returns (uint256 validationData)
+    {
+        uint256[4] memory publicKey = blsValidatorStorage[_userOp.sender];
+        bytes memory hashBytes = abi.encodePacked(_userOpHash);
+
+        uint256[2] memory message = BLS.hashToPoint(
+            BLS_DOMAIN,
+            hashBytes
+        );
+        uint256[2] memory decodedSignature = abi.decode(_userOp.signature, (uint256[2]));
+        (bool verified, bool callSuccess) = BLS.verifySingle(decodedSignature, publicKey, message);
+
+        if (verified && callSuccess) {
+            return 0;
+        }
+        // TODO: check if wallet recovered
+        return SIG_VALIDATION_FAILED;
+    }
+
+
+    function validateSignature(bytes32 hash, bytes calldata signature) public view override returns (uint256) {
+        uint256[4] memory publicKey = blsValidatorStorage[msg.sender];
+        uint256[2] memory decodedSignature = abi.decode(signature, (uint256[2]));
+
+        bytes memory hashBytes = abi.encodePacked(hash);
+        uint256[2] memory message = BLS.hashToPoint(
+            BLS_DOMAIN,
+            hashBytes
+        );
+        (bool verified, bool callSuccess) = BLS.verifySingle(decodedSignature, publicKey, message);
+
+        if (verified && callSuccess) {
+            return 0;
+        }
+        // TODO: check if wallet recovered
+        return SIG_VALIDATION_FAILED;
+    }
+}
