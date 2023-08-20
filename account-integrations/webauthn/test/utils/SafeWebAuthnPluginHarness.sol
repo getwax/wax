d314bc53f734e6de6aeb86c6af462b050afb9aab
JohnGuilding <jwguilding@gmail.com>
2023-08-20 10:48:10 +0100

    Add basic SafeWebAuthnPlugin tests with harness

---
+++ account-integrations/webauthn/test/utils/SafeWebAuthnPluginHarness.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import {UserOperation} from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {SafeWebAuthnPlugin} from "../../src/SafeWebAuthnPlugin.sol";
+
+/** Helper contract to expose internal functions for testing */
+contract SafeWebAuthnPluginHarness is SafeWebAuthnPlugin {
+    constructor(
+        address entryPointAddress,
+        uint256[2] memory pubKey
+    ) SafeWebAuthnPlugin(entryPointAddress, pubKey) {}
+
+    function exposed_validateSignature(
+        UserOperation calldata userOp,
+        bytes32 userOpHash
+    ) external returns (uint256) {
+        return _validateSignature(userOp, userOpHash);
+    }
+}
