d828737fe162b37c33eb2b2f97ddfb6e89bcd883
Blake Duncan <blakecduncan@gmail.com>
2023-11-03 09:48:26 -0400

    Export makeAccountWrapper and wrapper classes

--- account-integrations/sdk/src/accounts/AccountData.ts
+++ account-integrations/sdk/src/accounts/AccountData.ts
@@ -22,7 +22,6 @@ type AccountData = z.infer<typeof AccountData>;
 
 export default AccountData;
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export async function makeAccountWrapper(
   data: AccountData,
   provider: ethers.BrowserProvider,
