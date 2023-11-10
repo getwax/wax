4e642e0010c6a666bce4ede2de76a204bcf0fd7e
Andrew Morris <voltrevo@gmail.com>
2023-11-10 16:54:39 +1100

    Add HandleOpsCaller

---
+++ account-integrations/compression/src/HandleOpsCaller.sol
@@ -0,0 +1,111 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.7.0 <0.9.0;
+pragma abicoder v2;
+
+import {
+    IEntryPoint,
+    UserOperation
+} from "./I4337.sol";
+import {VLQ} from "./VLQ.sol";
+import {BitStack} from "./BitStack.sol";
+import {AddressRegistry} from "./AddressRegistry.sol";
+import {RegIndex} from "./RegIndex.sol";
+import {PseudoFloat} from "./PseudoFloat.sol";
+
+contract HandleOpsCaller {
+    IEntryPoint entryPoint;
+    address payable beneficiary;
+
+    AddressRegistry registry;
+
+    constructor(
+        IEntryPoint entryPointParam,
+        address payable beneficiaryParam,
+        AddressRegistry registryParam
+    ) {
+        entryPoint = entryPointParam;
+        beneficiary = beneficiaryParam;
+        registry = registryParam;
+    }
+
+    fallback(bytes calldata stream) external returns (bytes memory) {
+        uint256 len;
+        (len, stream) = VLQ.decode(stream);
+
+        uint256 bitStack;
+        (bitStack, stream) = VLQ.decode(stream);
+
+        UserOperation[] memory ops = new UserOperation[](len);
+
+        for (uint256 i = 0; i < len; i++) {
+            UserOperation memory op = ops[i];
+
+            (
+                op.sender,
+                stream,
+                bitStack
+            ) = decodeAddress(stream, bitStack);
+
+            (op.nonce, stream) = VLQ.decode(stream);
+
+            bool hasInitCode;
+            (hasInitCode, bitStack) = BitStack.pop(bitStack);
+
+            if (hasInitCode) {
+                (op.initCode, stream) = decodeBytes(stream);
+            }
+
+            (op.callData, stream) = decodeBytes(stream);
+            (op.callGasLimit, stream) = PseudoFloat.decode(stream);
+            (op.verificationGasLimit, stream) = PseudoFloat.decode(stream);
+            (op.preVerificationGas, stream) = PseudoFloat.decode(stream);
+            (op.maxFeePerGas, stream) = PseudoFloat.decode(stream);
+            (op.maxPriorityFeePerGas, stream) = PseudoFloat.decode(stream);
+
+            bool hasPaymaster;
+            (hasPaymaster, bitStack) = BitStack.pop(bitStack);
+
+            if (hasPaymaster) {
+                (op.paymasterAndData, stream) = decodeBytes(stream);
+            }
+
+            (op.signature, stream) = decodeBytes(stream);
+        }
+
+        entryPoint.handleOps(ops, beneficiary);
+
+        return hex"";
+    }
+
+    function decodeAddress(
+        bytes calldata stream,
+        uint256 bitStack
+    ) internal view returns (
+        address,
+        bytes calldata,
+        uint256
+    ) {
+        bool useRegistry;
+        (useRegistry, bitStack) = BitStack.pop(bitStack);
+
+        if (useRegistry) {
+            uint256 id;
+            (id, stream) = RegIndex.decode(stream);
+
+            return (registry.lookup(id), stream, bitStack);
+        }
+
+        return (address(bytes20(stream[:20])), stream[20:], bitStack);
+    }
+
+    function decodeBytes(
+        bytes calldata stream
+    ) internal pure returns (bytes memory, bytes calldata) {
+        uint256 len;
+        (len, stream) = VLQ.decode(stream);
+        bytes memory bytes_ = stream[:len];
+        stream = stream[len:];
+
+        return (bytes_, stream);
+    }
+}
---
+++ account-integrations/compression/test/HandleOpsCaller.t.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import {HandleOpsCaller} from "../src/HandleOpsCaller.sol";
+import {AddressRegistry} from "../src/AddressRegistry.sol";
+import {UserOperation, UserOpsPerAggregator} from "../src/I4337.sol";
+
+import {MockEntryPoint} from "./helpers/MockEntryPoint.sol";
+import {MockAggregator} from "./helpers/MockAggregator.sol";
+
+contract HandleOpsCallerTest is Test {
+    MockEntryPoint entryPoint;
+    AddressRegistry registry;
+    HandleOpsCaller handleOpsCaller;
+
+    bytes signature = (
+        hex"0102030405"
+    );
+
+    function setUp() public {
+        entryPoint = new MockEntryPoint();
+        registry = new AddressRegistry();
+
+        handleOpsCaller = new HandleOpsCaller(
+            entryPoint,
+            payable(address(this)),
+            registry
+        );
+
+        registry.register(address(0xdead));
+        registry.register(address(0xdead));
+        registry.register(address(0xdead));
+        registry.register(address(0xdead));
+
+        registry.register(address(0xa));
+        registry.register(address(0xb));
+        registry.register(address(0xc));
+    }
+
+    function test_one() public {
+        (bool success,) = address(handleOpsCaller).call(bytes.concat(
+            hex"01" // one operation
+            hex"09" // bit stack: (1)001
+                    // - 1: use registry for sender
+                    // - 0: no initCode
+                    // - 0: no paymaster
+                    // - 1: end of stack
+
+            hex"000004" // sender = 0xa via registry
+            hex"03" // nonce = 3
+
+            hex"0b" // 11 bytes of calldata
+            hex"000102030405060708090a" // calldata
+
+            hex"1f53" // callGasLimit = 67,100
+            hex"2500" // verificationGasLimit = 5,000
+            hex"2b0f" // preVerificationGas = 1,230,000
+
+            hex"3900" // maxFeePerGas = 0.001 gwei
+            hex"3100" // maxPriorityFeePerGas = 0.0001 gwei
+
+            hex"05"   // 5 bytes for signature
+            ,
+
+            signature
+        ));
+
+        assertEq(success, true);
+
+        UserOperation[] memory ops = new UserOperation[](1);
+
+        ops[0] = UserOperation({
+            sender: address(0xa),
+            nonce: 3,
+            initCode: hex"",
+            callData: hex"000102030405060708090a",
+            callGasLimit: 67_100,
+            verificationGasLimit: 5_000,
+            preVerificationGas: 1_230_000,
+            maxFeePerGas: 1_000_000,
+            maxPriorityFeePerGas: 100_000,
+            paymasterAndData: hex"",
+            signature: signature
+        });
+
+        assertEq(
+            entryPoint.handleOpsParams(),
+            abi.encode(ops, payable(address(this)))
+        );
+    }
+}
