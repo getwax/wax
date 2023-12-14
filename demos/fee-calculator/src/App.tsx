9b4f697532b9eb30a8de0157efed5206a61ea585
Andrew Morris <voltrevo@gmail.com>
2023-12-14 16:15:39 +1100

    Create fee-calculator using vite react-swc-ts template

---
+++ demos/fee-calculator/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
