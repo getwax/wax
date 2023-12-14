32473c07e8d0fde97339d732dadbc5cc5c27096c
Andrew Morris <voltrevo@gmail.com>
2023-12-14 16:22:30 +1100

    Set up linting

--- demos/fee-calculator/src/App.tsx
+++ demos/fee-calculator/src/App.tsx
@@ -1,19 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => <h1>Hello World</h1>;
 
-  return (
-    <>
-      <h1>Hello World</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-      </div>
-    </>
-  )
-}
-
-export default App
+export default App;
