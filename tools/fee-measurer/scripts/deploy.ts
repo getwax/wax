cee6094ae3621f4686dccc2ab3c54c254eb8d340
Andrew Morris <voltrevo@gmail.com>
2024-01-05 17:22:17 +1100

    In deploy.ts, just deploy

--- tools/fee-measurer/scripts/deploy.ts
+++ tools/fee-measurer/scripts/deploy.ts
@@ -8,22 +8,9 @@ async function main() {
   const [signer] = await ethers.getSigners();
 
   const ssf = await SafeSingletonFactory.init(signer);
-
   const feeMeasurer = await ssf.connectOrDeploy(FeeMeasurer__factory, []);
 
   console.log('FeeMeasurer deployed to:', await feeMeasurer.getAddress());
-
-  for (let size = 1n; size <= 3n; size++) {
-    console.log(size);
-    for (let i = 0; i < 3; i++) {
-      console.log("Gas prediction: ", await feeMeasurer.useGasOrdinaryGasUsed(size));
-      const receipt = (await (await feeMeasurer.useGas(size)).wait())!;
-      console.log("Gas used: ", receipt.gasUsed);
-    }
-  }
-
-  const receipt = (await (await feeMeasurer.fallback!({ data: "0x01" })).wait())!;
-  console.log(receipt.gasUsed);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
