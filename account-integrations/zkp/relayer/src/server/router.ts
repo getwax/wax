f9340d00dc1c6aa3eac26e62bb5aae5783c0af61
JohnGuilding <jwguilding@gmail.com>
2024-01-17 13:07:55 +0000

    Add heathcheck

---
+++ account-integrations/zkp/relayer/src/server/router.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response} from 'express';
+
+const router = express.Router();
+
+router.get("/health", (req: Request, res: Response) => {
+    return res.sendStatus(200);
+})
+
+export default router;
