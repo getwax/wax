e2055cbdf854a409b9bcc019b89d1a582fdd250f
Porco <porcorossoj89@gmail.com>
2024-03-27 21:33:36 +0400

    anon-aadhaar safe plugin

---
+++ packages/plugins/src/safe/SafeAnonAadhaarFactory.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+pragma abicoder v2;
+
+import {Safe} from "safe-contracts/contracts/Safe.sol";
+import {SafeProxyFactory} from "safe-contracts/contracts/proxies/SafeProxyFactory.sol";
+import {SafeProxy} from "safe-contracts/contracts/proxies/SafeProxy.sol";
+
+import {EntryPoint} from "account-abstraction/contracts/core/EntryPoint.sol";
+
+import {SafeAnonAadhaarPlugin} from "./SafeAnonAadhaarPlugin.sol";
+
+contract SafeAnonAadhaarFactory {
+    function create(
+        Safe safeSingleton,
+        EntryPoint entryPoint,
+        address owner,
+        uint256 saltNonce,
+        address _anonAadhaarAddr,
+        uint _userDataHash
+    ) external returns (SafeECDSAPlugin) {
+        bytes32 salt = keccak256(abi.encodePacked(owner, saltNonce));
+
+        Safe safe = Safe(
+            payable(new SafeProxy{salt: salt}(address(safeSingleton)))
+        );
+
+        address[] memory owners = new address[](1);
+        owners[0] = owner;
+
+        SafeAnonAadhaarPlugin plugin = new SafeAnonAadhaarPlugin{salt: salt}(
+            address(entryPoint),
+            _anonAadhaarAddr,
+            _userDataHash
+        );
+
+        safe.setup(
+            owners,
+            1,
+            address(plugin),
+            abi.encodeCall(SafeAnonAadhaarPlugin.enableMyself, (owner)),
+            address(plugin),
+            address(0),
+            0,
+            payable(address(0))
+        );
+
+        return SafeAnonAadhaarPlugin(address(safe));
+    }
+}
---
+++ packages/plugins/src/safe/SafeAnonAadhaarPlugin.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+pragma abicoder v2;
+
+import {Safe4337Base} from "./utils/Safe4337Base.sol";
+import {IEntryPoint, UserOperation} from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {UserOperation} from "account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+interface IAnonAadhaar {
+    function verifyAnonAadhaarProof(
+        uint identityNullifier,
+        uint userNullifier,
+        uint timestamp,
+        uint signal,
+        uint[8] memory groth16Proof
+    ) external view returns (bool);
+}
+
+interface ISafe {
+    function enableModule(address module) external;
+
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes memory data,
+        uint8 operation
+    ) external returns (bool success);
+}
+
+struct AnonAadhaarOwnerStorage {
+    address owner;
+}
+
+contract SafeAnonAadhaarPlugin is Safe4337Base {
+    mapping(address => AnonAadhaarOwnerStorage) public anonAadhaarOwnerStorage;
+
+    address public immutable myAddress; // Module address
+    address private immutable _entryPoint;
+
+    address public anonAadhaarAddr; // external contract managed by AnonAadhaar. it has verifyAnonAadhaarProof() method
+    uint public userDataHash; // the hash of unique and private user data extracted from Aadhaar QR code
+
+    address internal constant _SENTINEL_MODULES = address(0x1);
+
+    event OWNER_UPDATED(
+        address indexed safe,
+        address indexed oldOwner,
+        address indexed newOwner
+    );
+
+    constructor(
+        address entryPointAddress,
+        address _anonAadhaarAddr,
+        uint _userDataHash
+    ) {
+        myAddress = address(this);
+        _entryPoint = entryPointAddress;
+        anonAadhaarAddr = _anonAadhaarAddr;
+        userDataHash = _userDataHash;
+    }
+
+    function getOwner(address safe) external view returns (address owner) {
+        owner = anonAadhaarOwnerStorage[safe].owner;
+    }
+
+    function execTransaction(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable {
+        _requireFromEntryPoint();
+
+        bool success = _currentSafe().execTransactionFromModule(
+            to,
+            value,
+            data,
+            0
+        );
+
+        require(success, "tx failed");
+    }
+
+    function enableMyself(address ownerKey) public {
+        // Called during safe setup as a delegatecall. This is why we use `this`
+        // to refer to the safe instead of `msg.sender` / _currentSafe().
+
+        ISafe(address(this)).enableModule(myAddress);
+
+        // Enable the safe address with the defined key
+        bytes memory _data = abi.encodePacked(ownerKey);
+        SafeAnonAadhaarPlugin(myAddress).enable(_data);
+    }
+
+    function entryPoint() public view override returns (IEntryPoint) {
+        return IEntryPoint(_entryPoint);
+    }
+
+    function enable(bytes calldata _data) external payable {
+        address newOwner = address(bytes20(_data[0:20]));
+        address oldOwner = anonAadhaarOwnerStorage[msg.sender].owner;
+        anonAadhaarOwnerStorage[msg.sender].owner = newOwner;
+        emit OWNER_UPDATED(msg.sender, oldOwner, newOwner);
+    }
+
+    /// @dev Check if the timestamp is more recent than (current time - 3 hours)
+    /// @param timestamp: msg.sender address.
+    /// @return bool
+    function isLessThan3HoursAgo(uint timestamp) public view returns (bool) {
+        return timestamp > (block.timestamp - 3 * 60 * 60);
+    }
+
+    function _validateSignature(
+        UserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal view override returns (uint256 validationData) {
+        (
+            uint identityNullifier,
+            uint timestamp,
+            uint signal,
+            uint[8] memory groth16Proof,
+
+        ) = abi.decode(userOp.signature, (uint, uint, uint, uint[8]));
+
+        // see if the proof is fresh enough
+        require(isLessThan3HoursAgo(timestamp), "INVALID_TIMESTAMP");
+
+        // check the signal hasn't already been nullified
+        require(!signalNullifiers[signal], "DUPLICATED_NULLIFIER");
+
+        // make sure userOpHash == signal
+        require(uint(userOpHash) == signal, "INVALID_SIGNAL");
+
+        // verify proof throuugh AnonAadhaar and AnonAadhaarGroth16Verifier contracts
+        if (
+            !IAnonAadhaar(anonAadhaarAddr).verifyAnonAadhaarProof(
+                identityNullifier,
+                userDataHash,
+                timestamp,
+                signal,
+                groth16Proof
+            )
+        ) {
+            return SIG_VALIDATION_FAILED;
+        }
+
+        // store nullifier
+        signalNullifiers[signal] = true;
+        lastTimestamp = timestamp;
+        return 0;
+    }
+}
