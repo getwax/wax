6c298bde95d2d841cf3bd8bee7e6deaa7caab435
jacque006 <jacque006@users.noreply.github.com>
2024-04-03 01:11:40 -0600

    Email recovery demo frontend initial commit
    Use yarn create vite w/ React, Typescript
    Add WalletConnect provider lib

---
+++ packages/demos/email-recovery/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Email Recovery Demo</h1>
+      <button
+    </>
+  )
+}
+
+export default App
