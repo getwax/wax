c5fe43c974021b606d8e2da4ba3ce6b902b1ea90
Andrew Morris <voltrevo@gmail.com>
2024-04-04 10:34:45 +1100

    Record deployments

--- packages/plugins/script/deploy_all.ts
+++ packages/plugins/script/deploy_all.ts
@@ -1,5 +1,9 @@
+import fs from "fs/promises";
 import { ethers } from "ethers";
-import DeterministicDeployer from "../lib-ts/deterministic-deployer/DeterministicDeployer";
+import DeterministicDeployer, {
+  ContractFactoryConstructor,
+  DeployParams,
+} from "../lib-ts/deterministic-deployer/DeterministicDeployer";
 import {
   SimulateTxAccessor__factory,
   SafeProxyFactory__factory,
@@ -20,6 +24,9 @@ import {
 } from "../typechain-types";
 import makeDevFaster from "../test/e2e/utils/makeDevFaster";
 
+// 'test '.repeat(11) + 'absent'
+const testAbsentAddress = "0xe8250207B79D7396631bb3aE38a7b457261ae0B6";
+
 async function deploy() {
   const { NODE_URL, MNEMONIC } = process.env;
   const provider = new ethers.JsonRpcProvider(NODE_URL);
@@ -27,80 +34,71 @@ async function deploy() {
   const hdNode = ethers.HDNodeWallet.fromPhrase(MNEMONIC!);
   const wallet = new ethers.Wallet(hdNode.privateKey, provider);
 
-  const deployer = await DeterministicDeployer.init(wallet);
+  const recordingDeployer = await RecordingDeployer.init(wallet);
 
   const linkedAggregator = DeterministicDeployer.link(
     BLSSignatureAggregator__factory,
     [
       {
         "lib/account-abstraction/contracts/samples/bls/lib/BLSOpen.sol:BLSOpen":
-          deployer.calculateAddress(BLSOpen__factory, []),
+          recordingDeployer.deployer.calculateAddress(BLSOpen__factory, []),
       },
     ],
   );
 
-  const contractFactories = [
-    SimulateTxAccessor__factory,
-    TokenCallbackHandler__factory,
-    CompatibilityFallbackHandler__factory,
-    CreateCall__factory,
-    EntryPoint__factory,
-    MultiSend__factory,
-    MultiSendCallOnly__factory,
-    SignMessageLib__factory,
-    BLSOpen__factory,
-    linkedAggregator,
-    AddressRegistry__factory,
-  ];
+  const deployments = [
+    ["SimulateTxAccessor", SimulateTxAccessor__factory],
+    ["TokenCallbackHandler", TokenCallbackHandler__factory],
+    ["CompatibilityFallbackHandler", CompatibilityFallbackHandler__factory],
+    ["CreateCall", CreateCall__factory],
+    ["EntryPoint", EntryPoint__factory],
+    ["MultiSend", MultiSend__factory],
+    ["MultiSendCallOnly", MultiSendCallOnly__factory],
+    ["SignMessageLib", SignMessageLib__factory],
+    ["BLSOpen", BLSOpen__factory],
+    ["BLSSignatureAggregator", linkedAggregator],
+    ["AddressRegistry", AddressRegistry__factory],
+  ] as const;
 
-  for (const contractFactory of contractFactories) {
-    const contract = await deployer.connectOrDeploy(contractFactory, []);
-
-    const contractName = contractFactory.name.split("_")[0];
-    console.log(`deployed ${contractName} to ${await contract.getAddress()}`);
+  for (const [name, contractFactory] of deployments) {
+    await recordingDeployer.deploy(name, contractFactory, []);
   }
 
-  const handleOpsCaller = await deployer.connectOrDeploy(
+  const handleOpsCaller = await recordingDeployer.deploy(
+    "HandleOpsCaller",
     HandleOpsCaller__factory,
     [
-      deployer.calculateAddress(EntryPoint__factory, []),
+      recordingDeployer.deployer.calculateAddress(EntryPoint__factory, []),
       testAbsentAddress,
-      deployer.calculateAddress(AddressRegistry__factory, []),
+      recordingDeployer.deployer.calculateAddress(AddressRegistry__factory, []),
     ],
   );
 
-  console.log(
-    `deployed HandleOpsCaller to ${await handleOpsCaller.getAddress()}`,
-  );
-
-  const handleAggregatedOpsCaller = await deployer.connectOrDeploy(
+  const handleAggregatedOpsCaller = await recordingDeployer.deploy(
+    "HandleAggregatedOpsCaller",
     HandleAggregatedOpsCaller__factory,
     [
-      deployer.calculateAddress(EntryPoint__factory, []),
+      recordingDeployer.deployer.calculateAddress(EntryPoint__factory, []),
       testAbsentAddress,
-      deployer.calculateAddress(linkedAggregator, []),
-      deployer.calculateAddress(AddressRegistry__factory, []),
+      recordingDeployer.deployer.calculateAddress(linkedAggregator, []),
+      recordingDeployer.deployer.calculateAddress(AddressRegistry__factory, []),
     ],
   );
 
-  console.log(
-    `deployed HandleAggregatedOpsCaller to ${await handleAggregatedOpsCaller.getAddress()}`,
-  );
-
-  const safeDeployer = await DeterministicDeployer.initSafeVersion(wallet);
+  void handleOpsCaller;
+  void handleAggregatedOpsCaller;
 
   const safeContractFactories = [
-    SafeProxyFactory__factory,
-    SafeL2__factory,
-    Safe__factory,
-  ];
+    ["SafeProxyFactory", SafeProxyFactory__factory],
+    ["SafeL2", SafeL2__factory],
+    ["Safe", Safe__factory],
+  ] as const;
 
-  for (const contractFactory of safeContractFactories) {
-    const contract = await safeDeployer.connectOrDeploy(contractFactory, []);
-
-    const contractName = contractFactory.name.split("_")[0];
-    console.log(`deployed ${contractName} to ${await contract.getAddress()}`);
+  for (const [name, contractFactory] of safeContractFactories) {
+    await recordingDeployer.deployWithSafeDeployer(name, contractFactory, []);
   }
+
+  await recordingDeployer.finish();
 }
 
 deploy().catch((error: Error) => {
@@ -108,5 +106,68 @@ deploy().catch((error: Error) => {
   process.exitCode = 1;
 });
 
-// 'test '.repeat(11) + 'absent'
-const testAbsentAddress = "0xe8250207B79D7396631bb3aE38a7b457261ae0B6";
+class RecordingDeployer {
+  deployments: Record<string, string> = {};
+
+  private constructor(
+    public deployer: DeterministicDeployer,
+    public safeDeployer: DeterministicDeployer,
+  ) {}
+
+  static async init(wallet: ethers.Wallet): Promise<RecordingDeployer> {
+    const deployer = await DeterministicDeployer.init(wallet);
+    const safeDeployer = await DeterministicDeployer.initSafeVersion(wallet);
+
+    try {
+      await fs.rename(
+        "deployedAddresses.json",
+        "deployedAddresses.backup.json",
+      );
+    } catch (e) {
+      if ((e as { code: string }).code !== "ENOENT") {
+        throw e;
+      }
+    }
+
+    return new RecordingDeployer(deployer, safeDeployer);
+  }
+
+  async deploy<CFC extends ContractFactoryConstructor>(
+    name: string,
+    factory: CFC,
+    args: DeployParams<CFC>,
+  ) {
+    return this.deployImpl(name, factory, args, this.deployer);
+  }
+
+  async deployWithSafeDeployer<CFC extends ContractFactoryConstructor>(
+    name: string,
+    factory: CFC,
+    args: DeployParams<CFC>,
+  ) {
+    return this.deployImpl(name, factory, args, this.safeDeployer);
+  }
+
+  private async deployImpl<CFC extends ContractFactoryConstructor>(
+    name: string,
+    factory: CFC,
+    args: DeployParams<CFC>,
+    deployer: DeterministicDeployer,
+  ) {
+    const contract = await deployer.connectOrDeploy(factory, args);
+    const address = await contract.getAddress();
+    console.log(`Deployed ${name} to ${address}`);
+    this.deployments[name] = address;
+
+    return contract;
+  }
+
+  async finish() {
+    await fs.writeFile(
+      "deployedAddresses.json",
+      JSON.stringify(this.deployments, null, 2),
+    );
+
+    await fs.rm("deployedAddresses.backup.json", { force: true });
+  }
+}
