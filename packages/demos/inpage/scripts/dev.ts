b32b0863cea485647d2eda0071a81b8b3e99cfce
Andrew Morris <voltrevo@gmail.com>
2024-04-04 11:23:00 +1100

    Autodetect external node

--- packages/demos/inpage/scripts/dev.ts
+++ packages/demos/inpage/scripts/dev.ts
@@ -1,12 +1,34 @@
 #!/usr/bin/env tsx
 
+/* eslint-disable no-console */
+
 import concurrently from 'concurrently';
 import config from '../demo/config/config.ts';
 
 const tasks = ['vite'];
 
+let externalNode: boolean;
+
 if (config.rpcUrl === 'http://127.0.0.1:8545') {
-  tasks.push('yarn --cwd hardhat hardhat node');
+  try {
+    await fetch(config.rpcUrl);
+    externalNode = true;
+  } catch (e) {
+    if ((e as { code: string }).code !== 'ECONNREFUSED') {
+      throw e;
+    }
+
+    externalNode = false;
+    tasks.push('yarn --cwd hardhat hardhat node');
+  }
+} else {
+  externalNode = true;
+}
+
+if (externalNode) {
+  console.log(`Relying on external node: ${config.rpcUrl}`);
+} else {
+  console.log('Starting dev node');
 }
 
 await concurrently(tasks, { killOthers: 'failure' }).result;
