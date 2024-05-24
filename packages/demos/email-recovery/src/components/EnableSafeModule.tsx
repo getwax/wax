730095bfeba7f5e52c25ab97b51cdb5035d20c3c
jacque006 <jacque006@users.noreply.github.com>
2024-05-24 00:58:33 -0600

    Add partial Safe7579 adapter install flow

--- packages/demos/email-recovery/src/components/EnableSafeModule.tsx
+++ packages/demos/email-recovery/src/components/EnableSafeModule.tsx
@@ -1,57 +1,34 @@
-// import {
-//   installModule,
-//   getModule,
-//   getAccount,
-//   getClient,
-// } from '@rhinestone/module-sdk';
 import { ConnectKitButton } from "connectkit";
-import { encodeFunctionData } from 'viem';
 import { Button } from "./Button";
 import {
   useAccount,
   useReadContract,
   useWriteContract,
 } from "wagmi";
-import { safe7579, safe7579Launchpad, entryPoint } from "../../contracts.base-sepolia.json";
-import { abi as entryPointAbi } from "../abi/Safe7579Launchpad.json";
+import { safe7579, safeRecoveryModule } from "../../contracts.base-sepolia.json";
 import { abi as safeAbi } from "../abi/Safe.json";
-import { abi as launchpadAbi } from "../abi/Safe7579Launchpad.json";
+import { abi as safe7579Abi } from "../abi/Safe7579.json";
 import { useCallback, useContext, useEffect, useState } from "react";
 import { StepsContext } from "../App";
 import { STEPS } from "../constants";
 import Loader from "./Loader";
 import toast from "react-hot-toast";
-
-// TODO Move to env
-// const bundlerUrl = "https://public.stackup.sh/api/v1/node/base-sepolia";
+import { zeroAddress } from "viem";
 
 const EnableSafeModule = () => {
   const { address } = useAccount();
-
-  // const moduleClient = useMemo(() => getClient(chainId), [chainId]);
-  // const module = getModule({
-  //   module: moduleAddress,
-  //   data: initData,
-  //   type: moduleType,
-  // })
-
-  // // Get the executions required to install the module
-  // const executions = await installModule({
-  //   client,
-  //   account,
-  //   module,
-  // })
-
   const { writeContractAsync } = useWriteContract();
+
   const stepsContext = useContext(StepsContext);
   const [isEnableModalLoading, setIsEnableModuleLoading] = useState(false);
 
+  // TODO Update this to check Safe7579.isModuleInstalled
   const { data: isModuleEnabled, isLoading: isCheckModuleEnabledLoading } =
     useReadContract({
       address,
       abi: safeAbi,
       functionName: "isModuleEnabled",
-      args: [safe7579],
+      args: [safeRecoveryModule],
     });
 
   useEffect(() => {
@@ -73,77 +50,78 @@ const EnableSafeModule = () => {
     }
 
     toast("Please check Safe Website to complete transaction", {
-      icon: <img src={infoIcon} />,
+      // TODO Where is this infoIcon from?
+      // icon: <img src={infoIcon} />,
       style: {
         background: 'white'
       }
     })
 
-    const setupData = encodeFunctionData({
-      abi: launchpadAbi,
-      functionName: "initSafe7579",
+    console.debug("1", "Enable 7579 Module");
+
+    await writeContractAsync({
+      abi: safeAbi,
+      address,
+      functionName: "enableModule",
+      args: [safe7579]
+    });
+
+    console.debug("2", "Set as fallback handler");
+
+    await writeContractAsync({
+      abi: safeAbi,
+      address,
+      functionName: "setFallbackHandler",
+      args: [safe7579]
+    });
+
+    console.debug("3", "Initiliaze Safe w/ 7579 Adapter");
+
+    await writeContractAsync({
+      abi: safe7579Abi,
+      address: safe7579 as `0x{string}`,
+      functionName: "initializeAccount",
       args: [
-        safe7579,
-        [], // executors TODO We may need a default
-        [], // fallbacks
-        [], // hooks
-        [], // attester addr
-        0, // threshold
+        [], // Validators
+        [], // Executors TODO We may need to add a default
+        [], // Fallbacks
+        [], // Hooks
+        {
+          registry: zeroAddress, // TODO Set to deployed registry (if needed)
+          attesters: [],
+          threshold: 0,
+        },
       ],
     });
 
-    const initData = {
-      singleton: address,
-      owners: [], // TODO How can we get this?
-      threshold: 1, // Same here
-      setupTo: safe7579Launchpad,
-      setupData,
-      safe7579,
-      validators: [],
-      callData: "0x",
-      // TODO We might be able to use this to setup email recovery module in the same call
-      // Solidity example below,
-      //
-      // callData: abi.encodeCall(
-      //     IERC7579Account.execute,
-      //     (
-      //         ModeLib.encodeSimpleSingle(),
-      //         ExecutionLib.encodeSingle({
-      //             target: address(target),
-      //             value: 0,
-      //             callData: abi.encodeCall(MockTarget.set, (1337))
-      //         })
-      //     )
-      // )
-    };
+    // TODO Consider batching all of the above ^ via `safeAppsSDK.txs.send({ txs })` from @safe-global/safe-apps-sdk
+    // May also be able to use MultiCall3
 
-    const encodedSetupSafeCall = encodeFunctionData({
-      abi: launchpadAbi,
-      functionName: "setupSafe",
-      args: [initData],
-    });
+    // At this point, we should be setup for ERC-4337 & ERC-7579
 
-    const userOp = {};
+    // TODO This step currently reverts as it needs to be run via ERC-4337 Entrypoint (UserOp)
+    // Since this is likely the only UserOp we need to run, can directly submit to entrypoint
+    console.debug("4", "Install email recovery module");
 
-    // const userOP = {
-    //   sender: account,
-    //   nonce: 0,
-    //   initCode: "",
-    //   callData: "",
-    //   accountGasLimits: bytes32(abi.encodePacked(uint128(2e6), uint128(2e6))),
-    //   preVerificationGas: 2e6,
-    //   gasFees: bytes32(abi.encodePacked(uint128(2e6), uint128(2e6))),
-    //   paymasterAndData: bytes(""),
-    //   signature: abi.encodePacked(hex"41414141")
-    // };
+    // TODO Move to env
+    // const bundlerUrl = "https://public.stackup.sh/api/v1/node/base-sepolia";
 
-    // This can also be a a bundler RPC call instead to submit UserOp.
+    // TODO Move to ConfigureSafeModule component?
+    // TODO Check this is not already installed, can just directly submit to entrypoint
+    const executorModuleTypeId = 2; // Executor
     await writeContractAsync({
-      abi: entryPointAbi,
-      address: entryPoint as `0x{string}`,
-      functionName: "handleOps",
-      args: [[userOp], "0x" /* beneficiary */],
+      abi: safe7579Abi,
+      address: safe7579 as `0x{string}`,
+      functionName: "installModule",
+      args: [
+        executorModuleTypeId,
+        safeRecoveryModule,
+        "", // TODO Check this install data is no longer needed
+      ]
     });
+
+    console.debug("Done!");
+
   }, [address, writeContractAsync]);
 
   if (isCheckModuleEnabledLoading) {
